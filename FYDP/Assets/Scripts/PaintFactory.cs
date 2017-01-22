75b355dc865c8b10659b3da049b46c7c035751d1
Ruoyu Wang <r86wang@uwaterloo.ca>
2017-01-22 12:06:54 -0500

    Added wall paint

---
+++ FYDP/Assets/Scripts/PaintFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PaintFactory : MonoBehaviour
+{
+
+    public static PaintFactory Instance { get; private set; }
+
+    public void paintWalls(string name)
+    {
+
+        if(name == "clear")
+        {
+           // HoloToolkit.Unity.SpatialMappingManager.DrawVisualMeshes = false;
+        } else
+        {
+
+        }
+    }
+
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
