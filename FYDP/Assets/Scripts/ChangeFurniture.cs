d4e469eaa5d50d711ec33cf1fd46c1a6a3417e5b
Ruoyu Wang <r86wang@uwaterloo.ca>
2017-03-24 21:10:09 -0400

    Added tap to hold to change furniture functionality back

--- FYDP/Assets/Scripts/ChangeFurniture.cs
+++ FYDP/Assets/Scripts/ChangeFurniture.cs
@@ -1,36 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ChangeFurniture : MonoBehaviour {
 
     public GameObject furniture;
+    public static Dictionary<string, List<string>> furnitureNames = new Dictionary<string, List<string>>();
+
     void OnHoldStarted()
     {
         Vector3 position = GazeGestureManager.Instance.FocusedObject.transform.parent.position;
         Quaternion angle = GazeGestureManager.Instance.FocusedObject.transform.parent.rotation;
         print("hold started");
-        print(GazeGestureManager.Instance.FocusedObject.name);
-        if (GazeGestureManager.Instance.FocusedObject.name == "basic_table")
-        {
-            FurnitureFactory.Instance.DeleteFurniture(GazeGestureManager.Instance.FocusedObject.transform.parent.gameObject);
-            FurnitureFactory.Instance.CreateFurniture("BasicWoodTable", position, angle);
-        }
-        if (GazeGestureManager.Instance.FocusedObject.name == "basic_table_wood")
-        {
-            FurnitureFactory.Instance.DeleteFurniture(GazeGestureManager.Instance.FocusedObject.transform.parent.gameObject);
-            FurnitureFactory.Instance.CreateFurniture("BasicTable", position, angle);
-        }
 
+        FurnitureProperty property = GazeGestureManager.Instance.FocusedObject.transform.root.GetComponent<FurnitureMenuItemProperty>().furnitureProperty;
 
+        AssetLoader.Instance.DeleteFurniture(GazeGestureManager.Instance.FocusedObject.transform.parent.gameObject);
+        AssetLoader.Instance.onLoadAssetClick(property.bundle, getNextFurnitureName(property.bundle, property.assetName), position, angle);
     }
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void Start()
+    {
+        if(furnitureNames.Count == 0)
+        {
+            List<string> chairNames = new List<string>();
+            List<string> tableNames = new List<string>();
+            List<string> sofaNames = new List<string>();
+            List<string> storageNames = new List<string>();
+            List<string> drawerNames = new List<string>();
+            List<string> lightNames = new List<string>();
+            List<string> bedNames = new List<string>();
+            List<string> otherNames = new List<string>();
+
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.CHAIR_DICTIONARY)
+            {
+                chairNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.TABLE_DICTIONARY)
+            {
+                tableNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.DRAWER_DICTIONARY)
+            {
+                drawerNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.SOFA_DICTIONARY)
+            {
+                sofaNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.BED_DICTIONARY)
+            {
+                bedNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.STORAGE_DICTIONARY)
+            {
+                storageNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.LIGHT_DICTIONARY)
+            {
+                lightNames.Add(entry.Key);
+            }
+            foreach (KeyValuePair<string, FurnitureProperty> entry in SpawnList.OTHER_DICTIONARY)
+            {
+                otherNames.Add(entry.Key);
+            }
+            furnitureNames.Add("chairassets", chairNames);
+            furnitureNames.Add("drawerassets", tableNames);
+            furnitureNames.Add("tableassets", drawerNames);
+            furnitureNames.Add("sofaassets", sofaNames);
+            furnitureNames.Add("bedassets", bedNames);
+            furnitureNames.Add("storageassets", storageNames);
+            furnitureNames.Add("lightassets", lightNames);
+            furnitureNames.Add("otherassets", otherNames);
+        }
+    }
+
+    public string getNextFurnitureName(string bundle, string name)
+    {
+        List<string> names = furnitureNames[bundle];
+        int index = names.FindIndex(n => n == name);
+        if (index + 1 == names.Count)
+        {
+            return names[0];
+        }
+        else
+        {
+            return names[index + 1];
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
--- FYDP/Assets/Scripts/debugapp/AssetLoader.cs
+++ FYDP/Assets/Scripts/debugapp/AssetLoader.cs
@@ -16,11 +16,11 @@ public class AssetLoader : MonoBehaviour {
         Instance = this;
     }
 
-    public void onLoadAssetClick(string bundle, string name)
+    public void onLoadAssetClick(string bundle, string name, Vector3 position = default(Vector3), Quaternion angle = default(Quaternion))
     {
         if (isRemote == false)
         {
-            loadFromDisk(bundle, name);
+            loadFromDisk(bundle, name, position, angle);
         } 
         else
         {
@@ -46,6 +46,12 @@ public class AssetLoader : MonoBehaviour {
 
     }
 
+    public void DeleteFurniture(GameObject obj)
+    {
+        furntiureInstances.Remove(obj);
+        Destroy(obj);
+    }
+
     public void ResetAllFocus()
     {
         foreach (GameObject f in furntiureInstances)
@@ -54,17 +60,25 @@ public class AssetLoader : MonoBehaviour {
         }
     }
 
-    private void loadFromDisk(string bundle, string name)
+    private void loadFromDisk(string bundle, string name, Vector3 position = default(Vector3), Quaternion angle = default(Quaternion))
     {
-        Vector3 infront = new Vector3(0.5f, -0.5f, 2.0f);
-        Vector3 position = Camera.main.ViewportToWorldPoint(infront);
         SpawnList spawnlist = spawner.GetComponent<SpawnList>();
         GameObject furniture = spawnlist.findAsset(bundle, name);
+        Vector3 infront = new Vector3(0.5f, -0.5f, 2.0f);
+        if (position == default(Vector3))
+        {
+            position = Camera.main.ViewportToWorldPoint(infront);
+        }
+        if (angle == default(Quaternion))
+        {
+            angle = Quaternion.identity;
+        }
         if (furniture != null)
         {
-            GameObject furnitureInstance = Instantiate(furniture, position, Quaternion.identity);
+            GameObject furnitureInstance = Instantiate(furniture, position, angle);
             //furnitureInstance.AddComponent<TapToPlaceParent>();
             furnitureInstance.AddComponent<TapToPlace>();
+            furnitureInstance.AddComponent<ChangeFurniture>();
             furnitureInstance.SetActive(true);
             furntiureInstances.Add(furnitureInstance);
             FurnitureMenuItemProperty properties = furnitureInstance.AddComponent<FurnitureMenuItemProperty>();
